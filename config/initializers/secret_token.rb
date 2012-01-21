4e6c0f5501bcfe4d7fd35abf380169bbfe36f95c
Nik <nik@glstr.ru>
2012-01-21 16:22:20 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hellogift::Application.config.secret_token = 'df82ed10e49c2eb19cfcc6a1e8dab16c9721dbf9f725fff53fec790cdfa01e577afb05cd447adbfcd6fd49f6fa7f19885a8e89181fc3efb82d5a91d2f17bef09'
