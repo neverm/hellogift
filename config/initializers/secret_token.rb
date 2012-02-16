2281a261fbd5b5f2507f48f0f7fd390d4e22a3b3
Nik <nik@glstr.ru>
2012-02-16 23:31:32 +0300

    modified

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -6,6 +6,5 @@
 # no regular words or you'll be exposed to dictionary attacks.
 
 Hellogift::Application.config.secret_token = 'c6f244a4461bc00ce098709c2a9142ea927a66697ecc00f6a7eb350c62c090896e59d3408efdf2485f33011c828da2e92303022ef4eb7fcd920407729681c2dd'
-=======
 Hellogift::Application.config.secret_token = 'df82ed10e49c2eb19cfcc6a1e8dab16c9721dbf9f725fff53fec790cdfa01e577afb05cd447adbfcd6fd49f6fa7f19885a8e89181fc3efb82d5a91d2f17bef09'
 
