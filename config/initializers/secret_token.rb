fedb90c7c58ec7f46b1d5cc733b3f79f60749c05
Nik <nik@glstr.ru>
2012-02-16 23:27:16 +0300

    modified

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,8 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-<<<<<<< HEAD
+
 Hellogift::Application.config.secret_token = 'c6f244a4461bc00ce098709c2a9142ea927a66697ecc00f6a7eb350c62c090896e59d3408efdf2485f33011c828da2e92303022ef4eb7fcd920407729681c2dd'
 =======
 Hellogift::Application.config.secret_token = 'df82ed10e49c2eb19cfcc6a1e8dab16c9721dbf9f725fff53fec790cdfa01e577afb05cd447adbfcd6fd49f6fa7f19885a8e89181fc3efb82d5a91d2f17bef09'
->>>>>>> 9b8c3c65153097d1c7894f5a1bb22d8f43ad5183
+
