924640f6ae4a0aba54b104dfda379f6d2e7b6bd3
Nik <nik@glstr.ru>
2012-02-22 22:48:31 +0300

    modified

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,43 +1,15 @@
 # coding: utf-8
 class ListsController < ApplicationController
-	def new
+	def create
 		@title="Привет, подарок"
 				
-		if params[:text].nil? && session[:text].nil?
-			flash[:notice]="Сначала создайте список подарков!"	
-			redirect_to(root_path)	
-				
-		elsif !params[:text].nil? && session[:text].nil?
-			session[:list_text]=params[:list_text]
-			session[:text]=params[:text]
-
-			redirect_to(new_list_path)
-
-		elsif params[:text].nil? && !session[:text].nil?	 
-			if !session[:text].join.blank?
-				url=('a'..'z').to_a.shuffle[0..14].join
-				@l=List.new(:text => session[:list_text], :url => url)
-				@l.save
-
-				session[:text].each do |text|
-					@l.gifts.create(:text => text)
-				end
-				session[:list_url]=url	
-
-				session[:text] = nil
-				session[:list_text] = nil
-
-			else
-				flash[:notice]="Должен быть добавлен хотя бы один подарок!"	
-				redirect_to(root_path)		
-			end	
-					
-		end	
+		
 	end	
 
 	def set_user_email
 		if !params[:list][:user_email].blank?
-			@l.update_attribute(:user_email => params[:list][:user_email])
+			@l=List.find(params[:list]).keys
+			@l.update_attribute(params[:user_email])
 			flash[:notice]="Оk"
 		else
 			flash[:notice]="Error"	
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Hellogift::Application.routes.draw do
-
+  #match "/new", :to => "Lists#new", :as => "new_list"
+  #match "/set_user_email", :to => "Lists#set_user_email"
+  resources :lists, :except => :destroy do
+  	resources :gifts
+  end
+  
   root :to => "Pages#index"
-  match "/new", :to => "Lists#new", :as => "new_list"
-  match "/set_user_email", :to => "Lists#set_user_email"
 end
